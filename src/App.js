66d2b5d7e0737a0f5ba9d70d7c0c7d69a1e71d7b
faizaansheikh <sheikhfaizaan608@gmail.com>
2022-02-17 09:47:32 -0800

    completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-
-import './App.css';
-import cloud from "../src/cloud.jpg"
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "./App.css";
+// import cloud from "../src/cloud.jpg";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BsFillSunFill } from "react-icons/bs";
 import { GrSearch } from "react-icons/gr";
 import { FaCity } from "react-icons/fa";
@@ -9,14 +8,15 @@ import { FaTemperatureHigh } from "react-icons/fa";
 import { BiWind } from "react-icons/bi";
 import { WiHumidity } from "react-icons/wi";
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 function App() {
   const [weatherData, setWeatherData] = useState({});
   const [cityName, setCityName] = useState("");
   const [searchCityState, setSearchCityState] = useState("karachi");
+
   useEffect(() => {
     fetch(
-     `https://api.openweathermap.org/data/2.5/weather?q=${searchCityState}&units=metric&appid=bfd89c2a14fbbdad794eefbab688cd60`
+      `https://api.openweathermap.org/data/2.5/weather?q=${searchCityState}&units=metric&appid=bfd89c2a14fbbdad794eefbab688cd60`
     )
       .then((res) => res.json())
       .then((result) => {
@@ -25,68 +25,91 @@ function App() {
       .catch((err) => {
         console.log("0rrr", err);
       });
-  }, [cityName]);
+  }, [cityName,searchCityState]);
 
   const searchCity = (e) => {
+    e.preventDefault();
     setSearchCityState(cityName);
-    
+    setCityName("")
   };
-  
-  
+
   return (
     <>
- <div className='container-fluid bg'>
-   <h1 className='text-center head'>WEATHER</h1>
-   <div className='row'>
-  <div className='col-12 col-sm-12 col-md-6 maindiv'>
-    <div className='content'>
-   <span className='icon'> <BsFillSunFill size={150} color="yellow"/> 
-    </span>
-    
-    <h1 className='htwo'>{weatherData && weatherData.main && weatherData.main.temp}°C</h1> 
-    <span className='p'>
-    <p>Min_temp {weatherData && weatherData.main && weatherData.main.temp_min}</p> 
-    <p>Max_temp {weatherData && weatherData.main && weatherData.main.temp_max}</p>
-    </span>
+      <div className="container-fluid bg">
+        <h1 className="text-center head">WEATHER</h1>
+        <div className="row">
+          <div className="col-12 col-sm-12 col-md-6 maindiv">
+            <div className="content">
+              <span className="icon">
+                {" "}
+                <BsFillSunFill size={150} color="yellow" />
+              </span>
 
-    <h1>{weatherData && weatherData.name}</h1>
-    </div>
-  </div>
-  <div className='col-12 col-sm-12 col-md-6 cdiv'>
- <div className='iput'> <input
-          type="text"
-          value={cityName}
-          className="enter"
-          placeholder="Enter City name"
-          placeholder="Enter another city"
-          onChange={(event) => setCityName(event.target.value)}
-  />
-  <button onClick={searchCity}><GrSearch size={25}/></button>
-  </div>
-<div className='list'>
-<li>CITY: <FaCity/> {weatherData && weatherData.name} </li>
-      <li>Temp: <FaTemperatureHigh/> {weatherData && weatherData.main && weatherData.main.temp} </li>
-      <li>
-        Condition:<BiWind/>{" "}
-        {weatherData &&
-          weatherData.weather &&
-          weatherData.weather[0] &&
-          weatherData.weather[0].main}
-      </li>
-      <li>Feels like: {weatherData && weatherData.main && weatherData.main.feels_like} </li>
-      <li>Humidity:<WiHumidity size={30}/> {weatherData && weatherData.main && weatherData.main.humidity}%</li>
-      <li>Pressure: {weatherData && weatherData.main && weatherData.main.pressure} </li>
-      
+              <h1 className="htwo">
+                {weatherData && weatherData.main && weatherData.main.temp}°C
+              </h1>
+              <span className="p">
+                <p>
+                  Min_temp{" "}
+                  {weatherData && weatherData.main && weatherData.main.temp_min}
+                </p>
+                <p>
+                  Max_temp{" "}
+                  {weatherData && weatherData.main && weatherData.main.temp_max}
+                </p>
+              </span>
+
+              <h1>{weatherData && weatherData.name}</h1>
+            </div>
+          </div>
+          <div className="col-12 col-sm-12 col-md-6 cdiv">
+            <div className="iput">
+              {" "}
+              <input
+                type="text"
+                value={cityName}
+                className="enter"
+                placeholder="Enter City name"
+                onChange={(event) => setCityName(event.target.value)}
+              />
+              <button onClick={searchCity}>
+                <GrSearch size={25} />
+              </button>
+            </div>
+            <div className="list">
+              <li>
+                CITY: <FaCity /> {weatherData && weatherData.name}{" "}
+              </li>
+              <li>
+                Temp: <FaTemperatureHigh />{" "}
+                {weatherData && weatherData.main && weatherData.main.temp}{" "}
+              </li>
+              <li>
+                Condition:
+                <BiWind />{" "}
+                {weatherData &&
+                  weatherData.weather &&
+                  weatherData.weather[0] &&
+                  weatherData.weather[0].main}
+              </li>
+              <li>
+                Feels like:{" "}
+                {weatherData && weatherData.main && weatherData.main.feels_like}{" "}
+              </li>
+              <li>
+                Humidity:
+                <WiHumidity size={30} />{" "}
+                {weatherData && weatherData.main && weatherData.main.humidity}%
+              </li>
+              <li>
+                Pressure:{" "}
+                {weatherData && weatherData.main && weatherData.main.pressure}{" "}
+              </li>
+            </div>
+          </div>
+        </div>
       </div>
-  </div>
-  </div>
-  
- </div>
-
-
-
- 
-</>
+    </>
   );
 }
 
